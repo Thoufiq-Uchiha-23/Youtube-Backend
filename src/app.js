66221993fb0364f5931587062d59a01a475a6e4b
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-01-26 23:23:19 +0530

    app with routes

--- src/app.js
+++ src/app.js
@@ -18,6 +18,8 @@ app.use(cookieParser());
 import userRouter from './routes/user.routes.js';
 
 // routes declaration
-app.use("/users", userRouter);
+app.use("/api/v1/users", userRouter);
+
+// http://localhost:800/api/v1/users/register
 
 export { app }
